a510a392760dc52d3348518585c97e8987eca70b
YANN HAMDI <hamdiyann@gmail.com>
2020-04-11 19:24:56 +0100

    ajout mock

--- test_parsing.py
+++ test_parsing.py
@@ -39,3 +39,34 @@ def test_returning_cleaned_sentence():
     sentence = "TEST,openclassroom ailleurs:hamdi"
     pa = script.SentenceParse()
     assert pa.returning_cleaned_sentence(sentence) == "test openclassroom hamdi"
+
+
+def test_sending_to_api_handles_correct_result(monkeypatch):
+    FAKE_ADDRESS = "adresse de test, 7 cite de paradis 75010 paris, openclassroom"
+    FAKE_LAT = 49.0
+    FAKE_LNG = 3.0
+    # Definition du mock pour requests.get()
+    class Mockget:
+        def __init__(self,url):
+            pass
+
+        def json(self):
+	            return {
+	                  "results": [{"formatted_address": FAKE_ADDRESS, "geometry": { "location":
+	                  { "lat": FAKE_LAT, "lng": FAKE_LNG}
+
+	                  }}]
+	            }
+	# Definition du test pour search_around()
+    def mock_search_around(self, lat, lng):
+        pass
+    # patching the request.get pour simuler l'appel de l'api
+monkeypatch.setattr("parsing.requests.get", Mockget)
+    # patching de SentenceParse.search_around pour eviter de tester deux methondes en mêmme temps. We replace it with a method which doesnt do anything
+monkeypatch.setattr("parsing.SentenceParse.search_around", mock_search_around)
+    # we call the method sending_to_api
+    pa = script.SentenceParse()
+    pa.sending_to_api("petit test avec mock")
+    # Assert om the result's sending_to_api method
+    assert pa.address == FAKE_ADDRESS
+
