5b3130877f98daa87bcd831ccc7ae3de88a6f4b8
YANN HAMDI <hamdiyann@gmail.com>
2020-04-16 20:10:38 +0100

    essai de mocker mon api mais un souci

--- test_parsing.py
+++ test_parsing.py
@@ -93,6 +93,31 @@ def test_search_around_correct_result(monkeypatch):
     pa.search_around(F_LAT, F_LNG)
     assert pa.pageid == '9845754'
 
+def test_search_pageid_correct(monkeypatch):
+    "we mock our method search_page_id"
+    PAGE_ID = 565654
+    class MockReturning():
+        def __init__(self, url, params = {"format": "json", # format de la réponse
+                     "action": "query", # action à effectuer
+                       "prop": "extracts|info", # Choix des propriétés pour les pages requises
+                       "inprop": "url", # Fournit une URL complète, une URL de modification, et l’URL canonique de chaque page.
+                      "exchars": 1200, # Nombre de caractères à retourner
+                       "explaintext": 1, # Renvoyer du texte brut (éliminer les balises de markup)
+                       "pageids": PAGE_ID}):
+            pass
+        def json(self):
+            return{ 'query': {'pages': {PAGE_ID: {'extract':'ceci est un mock api',
+                                       'fullurl': 'https://fr.wikipedia.org/wiki/Academy_of_Art_University',}}
+
+            }
+    monkeypatch.setattr("parsing.requests.get", MockReturning)
+    pa = script.SentenceParse()
+    pa.search_page_id(7874647)
+    assert pa.extract == 'ceci est un mock'
+    
+   
+
+
 
 
 
