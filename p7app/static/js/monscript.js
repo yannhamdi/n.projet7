0b02c304560dc5028ca9067448867d2079243d84
YANN HAMDI <hamdiyann@gmail.com>
2020-04-26 23:43:31 +0100

    ajout fonctionnalité afficher question en dessous formulaire

--- p7app/static/js/monscript.js
+++ p7app/static/js/monscript.js
@@ -12,6 +12,13 @@ function postFormData(url, data){
 }
 form.addEventListener("submit", function(event){
 	event.preventDefault();
+	var nom = document.getElementById("question").value;
+	console.log(nom);
+	let b = document.querySelector("#question-form");
+	var newElt = document.createElement("p");
+	newElt.textContent = nom;
+    b.appendChild(newElt);
+
 	postFormData("/sendingServer",  new FormData(form))
 	.then(response => {
 		console.log(response.latitude)
