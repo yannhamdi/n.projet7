87a762b102900b497181e35eafae3aa4004706db
YANN HAMDI <hamdiyann@gmail.com>
2020-04-30 12:22:12 +0100

    essai afficher carte

--- p7app/static/js/monscript.js
+++ p7app/static/js/monscript.js
@@ -1,7 +1,6 @@
 
-
-
 let form = document.querySelector("#question-form");
+let b = document.querySelector("#question-form");
 function postFormData(url, data){
 	return fetch(url, {
 		method : "POST",
@@ -10,33 +9,33 @@ function postFormData(url, data){
 	.then(response => response.json())
     .catch(error => console.log(error));
 }
-
+function initMap(latitude, longitude){
+    	let mapElt = document.createElement("div");
+    	
+    	let myLatLng = {lat: latitude, lng: longitude};
+         mapElt.classList.add("map");
+         let map = new google.maps.Map(mapElt,{
+         center : myLatLng,
+         zoom: 8
+    	});	
+        b.appendChild(mapElt);
+    }
+function processPotAnswer(response){
+	var nom = document.getElementById("question").value;
+	var newElt = document.createElement("h5");
+	newElt.textContent = nom ;
+    b.appendChild(newElt);
+    initMap(response.latitude, response.longitude);
+}
 
 form.addEventListener("submit", function(event){
 	event.preventDefault();
-	var nom = document.getElementById("question").value;
-	let b = document.querySelector("#question-form");
-	var newElt = document.createElement("h5");
-	newElt.textContent = nom;
-    b.appendChild(newElt);
-
+	
 	postFormData("/sendingServer",  new FormData(form))
-	.then(
-	       function initMap(parentElt){
-         console.log("lololo")
-    	 let mapElt = document.createElement("div");
-         mapElt.classList.add("map");
-         let map = new google.maps.Map(mapElt,{
-        center :{lat: response.latitude, lng : response.longitude}, 
-    	zoom: 8
-    		})();
-        b.appendChild(mapElt);
-
-    
+	.then(response => console.log(response.latitude)
+	       )
 })
 
-})
-    
 
 
 
