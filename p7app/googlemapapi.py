346aa110cc7e8197ebbd39658ee847f19f9cc708
YANN HAMDI <hamdiyann@gmail.com>
2020-05-06 00:43:10 +0100

    pep8

--- p7app/googlemapapi.py
+++ p7app/googlemapapi.py
@@ -10,32 +10,24 @@ class TreatingApi:
     "class that will send a request to google map api"
     def __init__(self):
         "we initialize what is necessary for our requests"
-        self.url_1 = "https://maps.googleapis.com/maps/api/geocode/json?parameters"
-        self.sentence = ""
-        self.response = ""
-        self.url = ""
-        self.address = ""
-        self.response_json = ""
-        self.lat = 0
-        self.lng = 0
         self.dictio_coord = []
-        self.params = {}
     def sending_to_api(self, sentence):
         "function that sends the sentence to google api"
-        self.sentence = str(sentence)
-        self.params = {"address": self.sentence, "key": keyapi}
-        self.response = requests.get(self.url_1, params=self.params)
+        sentence = str(sentence)
+        url_1 = "https://maps.googleapis.com/maps/api/geocode/json?parameters"
+        params = {"address": sentence, "key": keyapi}
+        response = requests.get(url_1, params=params)
         try:
-            self.response_json = self.response.json()
+            response_json = response.json()
             #we select the address
-            self.address = (self.response_json["results"][0]["formatted_address"])
+            address = (response_json["results"][0]["formatted_address"])
             #we select our latitude
-            self.lat = (self.response_json["results"][0]["geometry"]["location"]["lat"])
+            lat = (response_json["results"][0]["geometry"]["location"]["lat"])
             #we select our longitude
-            self.lng = (self.response_json["results"][0]["geometry"]["location"]["lng"])
-            self.dictio_coord.append(self.address)
-            self.dictio_coord.append(self.lat)
-            self.dictio_coord.append(self.lng)
+            lng = (response_json["results"][0]["geometry"]["location"]["lng"])
+            self.dictio_coord.append(address)
+            self.dictio_coord.append(lat)
+            self.dictio_coord.append(lng)
             return self.dictio_coord
         except:
             print("Désolé mon petit loup je sais que je suis vieux et connais énormèment de \
