b2ae804beb5f9527101e7273580c6476a5e3d051
YANN HAMDI <hamdiyann@gmail.com>
2020-05-11 21:44:44 +0100

    requir

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn run:p7app
+
--- p7app/googlemapapi.py
+++ p7app/googlemapapi.py
@@ -20,7 +20,7 @@ class TreatingApi:
         "function that sends the sentence to google api"
         sentence = str(sentence)
         url_1 = "https://maps.googleapis.com/maps/api/geocode/json?parameters"
-        params = {"address": sentence, "key": os.getenv("KEY_API")}
+        params = {"address": sentence, "key": os.environ.get("KEY_API")}
         response = requests.get(url_1, params=params)
         if response.status_code == 200:
             response_json = response.json()
