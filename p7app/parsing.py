4ab008b058e231cf58b94200a58977c9b2165b02
YANN HAMDI <hamdiyann@gmail.com>
2020-05-12 11:04:50 +0100

    pep8

--- p7app/parsing.py
+++ p7app/parsing.py
@@ -11,11 +11,13 @@ class SentenceParse:
         self.uncleaned_sentence = []
         self.sentence = " "
         self.new_sentence = " "
+    
     def in_lower_case(self, sentence):
         "function that put the strings in lower case"
         self.sentence = str(sentence)
         self.sentence = self.sentence.lower()
         return self.sentence
+    
     def deleting_stop_words(self, sentence):
         """function that deletes the stop words"""
         self.sentence = str(sentence)
@@ -26,6 +28,7 @@ class SentenceParse:
                 self.uncleaned_sentence.append(word)
         self.new_sentence = " ".join(self.uncleaned_sentence)
         return self.new_sentence
+    
     def deleting_special(self, sentence):
         """function that deletes the special character"""
         self.sentence = str(sentence)
@@ -34,11 +37,13 @@ class SentenceParse:
         trantab = str.maketrans(intab, outtab)
         self.sentence = self.sentence.translate(trantab)
         return self.sentence
+    
     def deleting_several_spaces(self, sentence):
         """function that deletes spaces in case of double spaces"""
         self.sentence = str(sentence)
         self.sentence = self.sentence.replace("  ", " ")
         return self.sentence
+    
     def returning_cleaned_sentence(self, sentence):
         """fucntion that return the sentence cleaned"""
         self.sentence = str(sentence)
@@ -47,6 +52,7 @@ class SentenceParse:
         self.sentence = self.deleting_stop_words(self.sentence)
         self.sentence = self.deleting_several_spaces(self.sentence)
         return self.sentence
+
 def main():
     """initialize main function"""
     if __name__ == "__main__":
