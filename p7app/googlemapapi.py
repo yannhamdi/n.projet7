b0aeef4490f8423c193ee59649cde3ebb857a81e
YANN HAMDI <hamdiyann@gmail.com>
2020-05-15 00:05:43 +0100

    pep8

--- p7app/googlemapapi.py
+++ p7app/googlemapapi.py
@@ -6,6 +6,7 @@
 import os
 import requests
 
+
 class TreatingApi:
     """class that will send a request to google map api"""
     def __init__(self):
@@ -15,7 +16,7 @@ class TreatingApi:
         self.lat = 0
         self.lng = 0
         self.result = ""
-    
+
     def sending_to_api(self, sentence):
         """function that sends the sentence to google api"""
         sentence = str(sentence)
@@ -24,20 +25,23 @@ class TreatingApi:
         response = requests.get(url_1, params=params)
         if response.status_code == 200:
             response_json = response.json()
-            #we select the address
+            # we select the address
             self.address = (response_json["results"][0]["formatted_address"])
-            #we select our latitude
-            self.lat = (response_json["results"][0]["geometry"]["location"]["lat"])
-            #we select our longitude
-            self.lng = (response_json["results"][0]["geometry"]["location"]["lng"])
+            # we select our latitude
+            self.lat = (response_json["results"][0]
+                        ["geometry"]["location"]["lat"])
+            # we select our longitude
+            self.lng = (response_json["results"][0]["geometry"]
+                                     ["location"]["lng"])
             self.dictio_coord.append(self.address)
             self.dictio_coord.append(self.lat)
             self.dictio_coord.append(self.lng)
             return self.dictio_coord
         print("Désolé mon petit loup je sais que je suis vieux et connais énormèment de \
-            chose mais sur ce coup je ne vois pas ce que tu veux dire.")
+        chose mais sur ce coup je ne vois pas ce que tu veux dire.")
         self.result = "error"
         return self.result
+
 
 def main():
     """we initialize our main function"""
