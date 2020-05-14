9abf5cc229b5681e19c41770d5b97aeab1225e49
YANN HAMDI <hamdiyann@gmail.com>
2020-05-15 00:40:04 +0100

    pep8

--- p7app/views.py
+++ p7app/views.py
@@ -3,7 +3,7 @@
 import os
 from flask import Flask, render_template, request, jsonify
 
-from . import pybot as py 
+from . import pybot as py
 
 app = Flask(__name__)
 
@@ -12,7 +12,8 @@ app = Flask(__name__)
 @app.route('/p7homepage/')
 def index():
     """views that display our homepage"""
-    return render_template("p7homepage.html", api_key=os.environ.get("KEY_API"))
+    return render_template("p7homepage.html",
+                           api_key=os.environ.get("KEY_API"))
 
 
 @app.route('/sendingServer', methods=["POST"])
@@ -24,8 +25,10 @@ def sending_question():
     client_treatment.transformed_pageid_into_json(client_treatment.gps_lat
                                                   , client_treatment.gps_lng)
     client_treatment.transformed_info_js(client_treatment.pageid_for_js)
-    client_treatment.returning_dictionnary(client_treatment.info, client_treatment.url
-                                           , client_treatment.gps_adress, client_treatment.lat
+    client_treatment.returning_dictionnary(client_treatment.info ,
+                                           client_treatment.url,
+                                           client_treatment.gps_adress,
+                                           client_treatment.lat
                                            , client_treatment.lng)
     response = client_treatment.data_treated
     return jsonify(response)
