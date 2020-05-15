d04555365caf8c696863bf76a010cb87bda0d637
YANN HAMDI <hamdiyann@gmail.com>
2020-05-15 20:28:20 +0100

    test

--- p7app/static/js/monscript.js
+++ p7app/static/js/monscript.js
@@ -1,6 +1,7 @@
 
 let form = document.querySelector("#question-form");
 let b = document.querySelector("#question-form");
+let c = document.querySelector("#answer");
 var myImg = new Image();
 myImg.src = '/static/img/ajax-loader.gif';
 myImg.classList.add("icone");
@@ -34,7 +35,7 @@ function processPotAnswer(response){
         var newElt = document.createElement("h5");
         newElt.classList.add("answer");
         newElt.textContent = nom ;
-        b.appendChild(newElt);
+        c.appendChild(newElt, c.firstChild);
         var adAsked = document.createElement("h5");
         adAsked.classList.add("answer");
         adAsked.textContent = "Voici l'addresse demandée: " + response.addresse ;
