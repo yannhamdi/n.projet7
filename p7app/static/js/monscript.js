21aa9ad050981812c398cb1db538197b423d109a
YANN HAMDI <hamdiyann@gmail.com>
2020-05-15 20:51:08 +0100

    amelioration affichage

--- p7app/static/js/monscript.js
+++ p7app/static/js/monscript.js
@@ -1,7 +1,6 @@
 
 let form = document.querySelector("#question-form");
 let b = document.querySelector("#question-form");
-let c = document.querySelector("#answer");
 var myImg = new Image();
 myImg.src = '/static/img/ajax-loader.gif';
 myImg.classList.add("icone");
@@ -35,7 +34,7 @@ function processPotAnswer(response){
         var newElt = document.createElement("h5");
         newElt.classList.add("answer");
         newElt.textContent = nom ;
-        c.insertBefore(newElt, c.firstChild);
+        b.appendChild(newElt);
         var adAsked = document.createElement("h5");
         adAsked.classList.add("answer");
         adAsked.textContent = "Voici l'addresse demandée: " + response.addresse ;
